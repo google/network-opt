ef18cadb22d5365bcb5ef05ace08ad6e7566c1d4
Michael D. Moffitt <moffitt@google.com>
2021-12-23 21:15:42 -0600

    Update network_opt.cpp

--- network_opt.cpp
+++ network_opt.cpp
@@ -13,7 +13,7 @@ limitations under the License.
 
 #include "network_opt.h"
 
-PI_RATIO = Ratio(314159265358979,100000000000000);
+Ratio PI_RATIO = Ratio(314159265358979,100000000000000);
 
 Node& Node::operator[](Node& node) { children.push_back(&node); return *this; }
 
