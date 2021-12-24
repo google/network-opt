5b8efad703ffc50329ef6cdd1888aeff8a7caa5b
Michael D. Moffitt <moffitt@google.com>
2021-12-23 22:05:58 -0600

    Update network_opt_figures.cpp

--- network_opt_figures.cpp
+++ network_opt_figures.cpp
@@ -14,46 +14,46 @@ limitations under the License.
 #include "network_opt_utils.h"
 
 int main(int argc, char *argv[]) {
-  Node* network = NULL;
+  network_opt::Node* network = NULL;
 
   std::cout << "%%%%%%%% PAGE 3 LEFT TOP %%%%%%%%" << std::endl;
   network = &N()[N()[N()[NT(3)][NT(7)]][N()[NT(1)][NT(2)][NT(6)]][N()[NT(4)][NT(5)]]];
-  visualizer.visualize_schematic(std::cout, network, 7);
+  network_opt::visualizer.visualize_schematic(std::cout, network, 7);
   std::cout << std::endl;
   delete network;
 
   std::cout << "%%%%%%%% PAGE 3 RIGHT TOP %%%%%%%%" << std::endl;
   network = &N()[N()[N()[NT(3)][NT(7)]][N()[NT(1)][NT(2)][NT(5)]][N()[NT(4)][NT(6)]]];
-  visualizer.visualize_schematic(std::cout, network, 7);
+  network_opt::visualizer.visualize_schematic(std::cout, network, 7);
   std::cout << std::endl;
   delete network;
 
   std::cout << "%%%%%%%% PAGE 3 RIGHT BOTTOM %%%%%%%%" << std::endl;
   network = &N()[NT({1,3})][NT(7)[NT(6)[NT({2,5})]][NT(4)]];
-  visualizer.visualize_schematic(std::cout, network, 7);
+  network_opt::visualizer.visualize_schematic(std::cout, network, 7);
   std::cout << std::endl;
   delete network;
 
   std::cout << "%%%%%%%% PAGE 4 LEFT %%%%%%%%" << std::endl;
   network = &N()[NT({1,3})][NT(7)[N(6)[NT({2,5})]][NT(4)]];
-  visualizer.visualize_tree(std::cout, network, 7);
+  network_opt::visualizer.visualize_tree(std::cout, network, 7);
   std::cout << std::endl;
   delete network;
 
   std::cout << "%%%%%%%% PAGE 5 LEFT(A) %%%%%%%%" << std::endl;
   network = &N()[NT({1,3})][NT(7)[N()[NT({2,5,6})]][NT(4)]];
-  visualizer.visualize_tree(std::cout, network, 7);
+  network_opt::visualizer.visualize_tree(std::cout, network, 7);
   std::cout << std::endl;
   delete network;
 
   std::cout << "%%%%%%%% PAGE 5 LEFT(B) %%%%%%%%" << std::endl;
   network = &N()[N()[NT(1)][NT(3)][NT(4)]][NT(7)[N()[NT({2,5,6})]]];
-  visualizer.visualize_tree(std::cout, network, 7);
+  network_opt::visualizer.visualize_tree(std::cout, network, 7);
   std::cout << std::endl;
   delete network;
 
   std::cout << "%%%%%%%% PAGE 5 RIGHT TOP %%%%%%%%" << std::endl;
-  Tabulator tabulator(3);
+  network_opt::Tabulator tabulator(3);
   tabulator.tabulate(7);
   int entry256 = coder.encode({1,4,5});
   int entry134 = coder.encode({0,2,3});
@@ -87,21 +87,21 @@ int main(int argc, char *argv[]) {
 
   std::cout << "%%%%%%%% PAGE 5 RIGHT BOTTOM %%%%%%%%" << std::endl;
   network = &N()[N()[NT({2,5,6})]][NT(7)][N()[NT({1,3,4})]];
-  visualizer.visualize_tree(std::cout, network, 7);
+  network_opt::visualizer.visualize_tree(std::cout, network, 7);
   std::cout << std::endl;
   delete network;
 
   std::cout << "%%%%%%%% PAGE 7 (LEFT) %%%%%%%%" << std::endl;
   SERIES = E12_SERIES;
   network = &N()[N({0,5})][N()[N()[N(1)][N()[N(7)[N()[N(9)[N({2,11})]][N({4,8})]]]]][N()[N(3)][N({6,10})]]];
-  visualizer.visualize_schematic(std::cout, network, 12);
+  network_opt::visualizer.visualize_schematic(std::cout, network, 12);
   std::cout << std::endl;
   delete network;
 
   std::cout << "%%%%%%%% PAGE 7 (RIGHT) %%%%%%%%" << std::endl;
   SERIES = ONE_SERIES;
   network = &N()[N(0)][N(1)][N()[N()[N(2)][N(3)][N(4)][N(5)][N()[N(6)][N()[N(7)][N()[N()[N(8)][N(9)][N({10,11})]]]]]][N()[N(12)][N({13,14})]]];
-  visualizer.visualize_schematic(cout, network, 15);
+  network_opt::visualizer.visualize_schematic(cout, network, 15);
   std::cout << std::endl;
   delete network;
 }
