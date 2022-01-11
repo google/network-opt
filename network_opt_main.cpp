6bbf83b03e4ecba091a6bbe5184c076eb1801936
Michael D. Moffitt <moffitt@google.com>
2022-01-10 23:03:12 -0600

    Update network_opt_main.cpp

--- network_opt_main.cpp
+++ network_opt_main.cpp
@@ -11,16 +11,25 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-#include "network_opt.h"
+#include "network_local.h"
+#include "stdlib.h"
 
 int main(int argc, char *argv[]) {
   std::cout << " Command:";
   for (int i = 0; i < argc; ++i) std::cout << " " << argv[i];
   std::cout << std::endl;
-  unsigned int b = atoi(argv[1]), t = atoi(argv[2]);
+  std::string solver = argv[1];
+  unsigned int b = atoi(argv[2]), t = atoi(argv[3]);
   network_opt::Problem problem = network_opt::Problem::from_argv(argv);
   network_opt::Params params(b, t);
-  network_opt::Solver solver(params);
-  network_opt::Node* network = solver.solve(problem);
-  network_opt::print_summary(std::cout, problem, network, "");
+  if (solver == "opt") {
+    network_opt::Solver solver(params);
+    network_opt::Node* network = solver.solve(problem);
+    network_opt::print_summary(std::cout, problem, network, "");
+  } else if (solver == "local") {
+    srand(2022);
+    network_opt::LocalSolver solver(params);
+    network_opt::Node* network = solver.solve(problem);
+    network_opt::print_summary(std::cout, problem, network, "");
+  }
 }
